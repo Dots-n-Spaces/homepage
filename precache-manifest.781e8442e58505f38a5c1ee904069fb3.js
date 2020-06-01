a514c7cbcfac06d1aa9881c10b609d88b784950e
Viktor Lesyk <vee.lesyk@gmail.com>
2020-06-01 16:14:45 +0200

    Updates

---
+++ precache-manifest.781e8442e58505f38a5c1ee904069fb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e251589a64c069cd47b928d97fef00ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "543b9808fe11d0ea62f5",
+    "url": "/static/css/main.8238c64b.chunk.css"
+  },
+  {
+    "revision": "e216811372754b3ab9f9",
+    "url": "/static/js/2.f0147a2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f0147a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543b9808fe11d0ea62f5",
+    "url": "/static/js/main.139dec56.chunk.js"
+  },
+  {
+    "revision": "77b7fea5572c627fed44",
+    "url": "/static/js/runtime-main.bd2298fb.js"
+  },
+  {
+    "revision": "5b761f2d1e4259ea6ac7ab3ebf7f3c49",
+    "url": "/static/media/Lato-Light.5b761f2d.ttf"
+  },
+  {
+    "revision": "7ff4438405bfb9fe87b606ca356ba6a0",
+    "url": "/static/media/RobotoCondensed-Bold.7ff44384.ttf"
+  }
+]);
