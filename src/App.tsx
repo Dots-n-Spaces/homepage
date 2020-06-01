03b38cc734896aef6f24e47ad2dd649433348023
Viktor Lesyk <vee.lesyk@gmail.com>
2020-06-01 16:19:09 +0200

    Fix: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import { Router } from "react-router";
+import { createBrowserHistory } from "history";
 
 import "./styles/App.css";
 import { SolidBalance } from "./components/SolidBalance";
@@ -7,10 +9,12 @@ import { SnapAssist } from "./components/SnapAssist";
 import { Loki } from "./components/Loki";
 import { NoMatch } from "./components/NoMatch";
 
+const history = createBrowserHistory();
+
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route exact path="/">
             <SolidBalance />
